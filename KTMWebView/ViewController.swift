1115ec8f24c36b53cccda39cb2abc83f029456f5
Kishore Thejasvi <kishore.thejasvi@gmail.com>
2017-01-31 08:25:45 +0530

    Initial Commit

---
+++ KTMWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KTMWebView
+//
+//  Created by Kishore Thejasvi on 31/01/2017.
+//  Copyright © 2017 Kishore Thejasvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
